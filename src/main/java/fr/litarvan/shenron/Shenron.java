760552138f3a2f479059c5607593c090e3be9a58
Adrien Navratil <adrien1975@live.fr>
2017-07-15 23:47:32 +0200

    Sudo command, license, fixes

--- src/main/java/fr/litarvan/shenron/Shenron.java
+++ src/main/java/fr/litarvan/shenron/Shenron.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2016-2017 Adrien 'Litarvan' Navratil
+ *
+ * This file is part of Shenron.
+ *
+ * Shenron is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shenron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shenron.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.litarvan.shenron;
 
 import java.io.File;
@@ -76,6 +94,10 @@ public class Shenron implements IBot
                 .description("Affiche la liste des commandes")
                 .register();
 
+        commands.make("add", (context, args) -> context.sendMessage(configs.at("shenron.add-link")))
+                .description("Affiche le lien d'ajout du bot")
+                .register();
+
         music();
 
         commands.group().middlewares(SupportMiddleware.class).apply(() -> {
@@ -99,11 +121,11 @@ public class Shenron implements IBot
                 .register();
 
         commands.make("timer <action:start|stop>", CommandTimer.class)
-                .description("Display an animated emoji timer on this channel")
+                .description("Affiche un timer **animé** en emoji")
                 .register();
 
-        commands.make("add", (context, args) -> context.sendMessage(configs.at("shenron.add-link")))
-                .description("Affiche le lien d'ajout du bot")
+        commands.make("sudo <target:user> <message:string...>", CommandSudo.class)
+                .description("Envoi un message en simulant qu'il a été envoyé par un autre utilisateur")
                 .register();
 
         Command clear = commands.make("clear <amount:number>", CommandClear.class)
--- src/main/java/fr/litarvan/shenron/command/CommandSudo.java
+++ src/main/java/fr/litarvan/shenron/command/CommandSudo.java
@@ -16,35 +16,44 @@
  * You should have received a copy of the GNU General Public License
  * along with Shenron.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*
+ * Copyright 2016-2017 Adrien 'Litarvan' Navratil
+ *
+ * This file is part of Shenron.
+ *
+ * Shenron is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shenron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shenron.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.litarvan.shenron.command;
 
+import fr.litarvan.shenron.MessageSudo;
+import java.util.Map;
 import net.dv8tion.jda.core.Permission;
+import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
 import org.krobot.command.CommandContext;
 import org.krobot.command.CommandHandler;
 import org.krobot.command.SuppliedArgument;
-import java.util.Map;
-import fr.litarvan.shenron.MessageSudo;
-import org.jetbrains.annotations.NotNull;
 import org.krobot.permission.BotRequires;
+import org.krobot.permission.UserRequires;
 
 @BotRequires({Permission.MANAGE_WEBHOOKS})
-public class CommandSimpleLink implements CommandHandler
+@UserRequires({Permission.ADMINISTRATOR})
+public class CommandSudo implements CommandHandler
 {
-    private String link;
-
-    public CommandSimpleLink(String link)
-    {
-        this.link = link;
-    }
-
     @Override
     public void handle(@NotNull CommandContext context, @NotNull Map<String, SuppliedArgument> args) throws Exception
     {
-        MessageSudo.send(context.getUser(), context.getChannel(), link);
-    }
-
-    public String getLink()
-    {
-        return link;
+        MessageSudo.send(args.get("target").getAsUser(), context.getChannel(), Strings.join(args.get("message").getAsStringList(), ' '));
     }
 }
