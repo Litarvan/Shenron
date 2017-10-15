0480a5a1ebef0fc516821532f5c834b3efe662e0
Adrien Navratil <adrien1975@live.fr>
2017-10-15 12:40:46 +0200

    Version increment

--- src/main/java/fr/litarvan/shenron/Shenron.java
+++ src/main/java/fr/litarvan/shenron/Shenron.java
@@ -33,7 +33,7 @@ import org.krobot.module.Include;
 @Bot(name = "Shenron", version = Shenron.VERSION, author = "Adrien 'Litarvan' Navratil")
 public class Shenron extends KrobotModule
 {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "3.0.0";
 
     @Inject
     private ConfigProvider configs;
